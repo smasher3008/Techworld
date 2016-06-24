f82ec52c6b656d73c35a68317f391074171e65c3
smasher3008 <hitengupta@rocketmail.com>
2016-06-24 14:34:11 +0530

    updating

---
+++ src/main/java/UserModel/User.java
@@ -0,0 +1,97 @@
+package UserModel;
+
+import java.io.Serializable;
+
+import javax.management.relation.Role;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class User implements Serializable {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int UserId;
+	
+	private Boolean active;
+	private Role "1" ;
+	private String name;
+	
+	
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getConfirmPassword() {
+		return ConfirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		ConfirmPassword = confirmPassword;
+	}
+
+	public int getContactno() {
+		return Contactno;
+	}
+
+	public void setContactno(int contactno) {
+		Contactno = contactno;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	private String Username;
+	private String email;
+	private String Password;
+	
+	@Transient
+	private String ConfirmPassword;
+	
+	private int Contactno;
+	private String Address;
+
+	public int getUserId() {
+		return UserId;
+	}
+
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/UserModel/UserDao.java
@@ -0,0 +1,13 @@
+package UserModel;
+
+import javax.persistence.Entity;
+
+@Entity
+public interface UserDao {
+
+	public void insert(User u);
+	
+	
+	
+	
+}
